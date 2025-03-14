a761da20e9070c33dfc19c193786b0f6228dc27b
mendix.service <mendix@example.com>
2025-03-14 17:54:14 +0700

    need to fix circular import

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,18 @@
 __version__ = "0.0.1"
+
+from telegram.ext import Application, Defaults
+from src.modules.environment import TOKEN
+import pytz
+from src.modules.utils.logger import get_logger
+
+
+LOGGER = get_logger(name=__name__)
+
+builder = Application.builder()
+builder.token(TOKEN)
+builder.defaults(Defaults(tzinfo=pytz.timezone('Asia/Jakarta')))
+app = builder.build()
+
+
+LOGGER.info("INITIALIZING Telegram Pi Wallet Bot")
+
--- src/modules/environment.py
+++ src/modules/environment.py
@@ -1,11 +1,27 @@
 from dotenv import load_dotenv
+from src.modules.utils.logger import get_logger
+import json
 import os
 
 load_dotenv(override=True)
 
+
+LOGGER = get_logger(__name__)
+
 TOKEN = os.getenv("TOKEN") 
 BOT_USERNAME  = os.getenv("BOT_USERNAME") 
 TIMEOUT_LIMIT = int(os.getenv("TIMEOUT_LIMIT"))
+REMOVE_TAG = bool(os.getenv("REMOVE_TAG"))
+
+# load json file
+config_name = "chat_list.json"
+CONFIG = {}
+if not os.path.isfile(config_name):
+    LOGGER.error("No chat_list.json config file found! Exiting...")
+    exit(1)
+with open(config_name, "r") as data:
+    CONFIG = json.load(data)
+
 
 def get_all_env():
     data = [f"{i}: {j}" for i,j in os.environ.items()]
